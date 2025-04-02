0ea63daa5b028d971d8b14b4ddcbcc46e33306d2
duytungqua <quangduy.hcmut@gmail.com>
2025-04-02 23:48:38 +0700

    feat: impl factory pattern in java

--- src/creational/factory/Factory.java
+++ src/creational/factory/Factory.java
@@ -1,5 +1,15 @@
 package creational.factory;
 
 public class Factory {
-    //
+    public Shape getShape(String shapeType) {
+        if (shapeType == null) {
+            return null;
+        }
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+            return new Ractangle();
+        }
+        return null;
+    }
 }
---
+++ src/creational/factory/Ractangle.java
@@ -0,0 +1,9 @@
+package creational.factory;
+
+public class Ractangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Inside Ractangle::draw() method.");
+    }
+    
+}
